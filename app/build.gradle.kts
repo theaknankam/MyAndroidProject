0024abd6cbbe529ef3bb84e10e52c761ef5930a1
Thea Elisabeth Kamdoum-Nankam <thea_elisabeth.kamdoum-nankam@smail.th-koeln.de>
2026-05-14 11:38:06 +0200

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,21 +38,28 @@ android {
         compose = true
     }
 }
-
 dependencies {
+
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation("androidx.compose.material:material-icons-extended")
+
     testImplementation(libs.junit)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
