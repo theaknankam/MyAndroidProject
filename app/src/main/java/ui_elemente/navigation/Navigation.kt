a4a25cc36d64e5e290e5dd13ea795180e999749b
dienniedi <diana.sukiiazova@smail.th-koeln.de>
2026-05-18 15:04:19 +0200

    Navigation.kt aktualisiert

--- app/src/main/java/ui_elemente/navigation/Navigation.kt
+++ app/src/main/java/ui_elemente/navigation/Navigation.kt
@@ -1,51 +1,29 @@
 package ui_elemente.navigation
+
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-
-import androidx.lifecycle.viewmodel.compose.viewModel
-
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-
 import kotlinx.coroutines.delay
-
-import com.example.carsharing_app.R
-
-import ui_elemente.screens.Autoauswahl
-import ui_elemente.screens.CreateRideScreen
-import ui_elemente.screens.HomeScreen
-import ui_elemente.screens.LoadingScreen
-import ui_elemente.screens.LoginScreen
-import ui_elemente.screens.ProfileScreen
-import ui_elemente.screens.RideDetailsScreen
-
-import ui_elemente.viewModel.VehiculeViewmodel
 import kotlin.time.Duration.Companion.milliseconds
+import ui_elemente.screens.LoadingScreen
 
 @Composable
 fun Navigation() {
 
-    // Contrôleur de navigation
     val navController = rememberNavController()
 
-    // Permet de savoir sur quelle page on est actuellement
     val navBackStackEntry by navController.currentBackStackEntryAsState()
+
     val currentRoute = navBackStackEntry?.destination?.route
 
-    // Etat du Loading Screen
     var isLoading by rememberSaveable {
         mutableStateOf(true)
     }
@@ -56,8 +34,14 @@ fun Navigation() {
     }
 
     if (isLoading) {
-
         LoadingScreen()
+        return
+    }
+
+    // Login = pas de barre de navigation
+    if (currentRoute == "login" || currentRoute == null) {
+
+        AppNavHost(navController)
 
     } else {
 
@@ -69,7 +53,6 @@ fun Navigation() {
 
                     item(
 
-                        // Icône de l'onglet
                         icon = {
                             Icon(
                                 painter = painterResource(destination.icon),
@@ -78,45 +61,23 @@ fun Navigation() {
                             )
                         },
 
-                        // Texte de l'onglet
                         label = {
                             Text(destination.label)
                         },
 
-                        // Indique quel onglet est sélectionné
-                        selected =
-                            currentRoute ==
-                                    when (destination) {
-                                        AppDestinations.HOME -> "home"
-                                        AppDestinations.MESSAGE -> "createRide"
-                                        AppDestinations.RIDE -> "autoauswahl"
-                                        AppDestinations.PROFILE -> "profile"
-                                    },
+                        selected = currentRoute == destination.route,
 
-                        // Que faire lorsqu'on clique sur un onglet
                         onClick = {
 
-                            when (destination) {
+                            navController.navigate(destination.route) {
 
-                                AppDestinations.HOME ->
-                                    navController.navigate("home") {
-                                        launchSingleTop = true
-                                    }
+                                launchSingleTop = true
 
-                                AppDestinations.MESSAGE ->
-                                    navController.navigate("createRide") {
-                                        launchSingleTop = true
-                                    }
+                                restoreState = true
 
-                                AppDestinations.RIDE ->
-                                    navController.navigate("autoauswahl") {
-                                        launchSingleTop = true
-                                    }
-
-                                AppDestinations.PROFILE ->
-                                    navController.navigate("profile") {
-                                        launchSingleTop = true
-                                    }
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
                             }
                         }
                     )
@@ -125,59 +86,8 @@ fun Navigation() {
 
         ) {
 
-            // Ici on déclare tous les écrans de l'application
-            NavHost(
-                navController = navController,
-                startDestination = "login"
-            ) {
+            AppNavHost(navController)
 
-                composable("login") {
-                    LoginScreen(
-                        onLoginSuccess = {
-                            navController.navigate("home")
-                        }
-                    )
-                }
-
-                composable("home") {
-                    HomeScreen()
-                }
-
-                composable("createRide") {
-                    CreateRideScreen(
-                        navController = navController)
-                }
-
-                composable("autoauswahl") {
-
-                    val viewModel: VehiculeViewmodel = viewModel()
-
-                    Autoauswahl(
-                        viewModel = viewModel,
-                        navController = navController
-                    )
-                }
-
-                composable("profile") {
-                    ProfileScreen()
-                }
-
-                composable("rideDetails") {
-                    RideDetailsScreen()
-                }
-            }
         }
     }
-}
-
-
-
-enum class AppDestinations(
-    val label: String,
-    val icon: Int,
-) {
-    HOME("Home", R.drawable.ic_home),
-    RIDE("Ride", R.drawable.ic_car_white),
-    MESSAGE("Message", R.drawable.ic_message_white),
-    PROFILE("Profile", R.drawable.ic_account_box)
 }
